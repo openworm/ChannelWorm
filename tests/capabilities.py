0083ceb94d2da60d415540aedf180aea3fc6468c
Travis Jacobs <trav221@gmail.com>
2015-05-23 17:08:19 -0230

    Adds Generates_IV_Curve.py

---
+++ tests/capabilities.py
@@ -0,0 +1,21 @@
+# this file will hold the Capabilities that our Models can have
+
+import sciunit
+
+class Generates_IV_Curve(sciunit.Capability, Receives_Current, Generates_Membrane_Potential):
+    """ This model can generate and IV curve when simulated. """
+
+    def generate_iv_curve(self):
+        raise NotImplementedError("generate_iv_curve not implemented")
+
+class Receives_Current(sciunit.Capability):
+    """ This model can receive a current when simluated. """
+
+    def receives_current(self):
+        raise NotImplementedError("receives_current not implemented")
+
+class Generates_Membrane_Potential(sciunit.Capability):
+    """ This model can produce a membrane potential when simluated. """
+
+    def generates_membrane_potential(self):
+        raise NotImplementedError("generates_membrane_potential not implemented")
