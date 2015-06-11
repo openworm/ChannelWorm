fdc886d08bff0647caf3adce59da114f6bfd0a5c
Travis Jacobs <trav221@gmail.com>
2015-06-11 14:03:37 -0230

    Trying to locate resource for PyNeuroML again

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
         'cypy',
         'sciunit',
         'PyOpenWorm',
-        'PyNeuroML'
+        'PyNeuroML>=0.0.6'
     ],
     dependency_links=[
         'git+https://github.com/scidash/sciunit.git#egg=sciunit',
         'git+https://github.com/openworm/PyOpenWorm.git#egg=PyOpenWorm',
-        'git+https://github.com/NeuroML/pyNeuroML.git@5aeab1243567d9f4a8ce16516074dc7b93dfbf37'
+        'git+https://github.com/NeuroML/pyNeuroML.git#egg=PyNeuroML@5aeab1243567d9f4a8ce16516074dc7b93dfbf37'
     ]
 )
