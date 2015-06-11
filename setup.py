fda74b21a8b366814b18b7bf3f36b1ce611d006d
Travis Jacobs <trav221@gmail.com>
2015-06-11 13:53:55 -0230

    Fix pyNeuroML dependecy link to go to specific commit

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     dependency_links=[
         'git+https://github.com/scidash/sciunit.git#egg=sciunit',
         'git+https://github.com/openworm/PyOpenWorm.git#egg=PyOpenWorm',
-        'git+https://github.com/NeuroML/pyNeuroML.git#egg=PyNeuroML@5aeab1243567d9f4a8ce16516074dc7b93dfbf37'
+        'git+https://github.com/NeuroML/pyNeuroML.git@5aeab1243567d9f4a8ce16516074dc7b93dfbf37'
     ]
 )
