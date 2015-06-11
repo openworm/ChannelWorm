fad49c4585e6d9143feb7ef7a96f7e72c0d79353
Travis Jacobs <trav221@gmail.com>
2015-06-11 16:23:22 -0230

    Add dependencies for neurotune implementation

--- setup.py
+++ setup.py
@@ -7,11 +7,16 @@ setup(
         'cypy',
         'sciunit',
         'PyOpenWorm',
-        'PyNeuroML'
+        'PyNeuroML',
+        'inspyred',
+        'pyelectro',
+        'neurotune'
     ],
     dependency_links=[
         'git+https://github.com/scidash/sciunit.git#egg=sciunit',
         'git+https://github.com/openworm/PyOpenWorm.git#egg=PyOpenWorm',
-        'git+https://github.com/NeuroML/pyNeuroML.git#egg=PyNeuroML'
+        'git+https://github.com/NeuroML/pyNeuroML.git#egg=PyNeuroML',
+        'git+https://github.com/pgleeson/pyelectro.git#egg=pyelectro',
+        'git+https://github.com/pgleeson/neurotune.git#egg=neurotune'
     ]
 )
