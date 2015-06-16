ce593c7d25e48b147e914efbb19ace2dc6191ee8
Joe Bowen <joe.b.bowen@gmail.com>
2015-06-16 17:02:57 -0600

    Fix web app. #79

---
+++ channelworm/.openshift/cron/minutely/updated_from_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/app-root/runtime/repo
+
+# rm -rf ChannelWorm
+
+# git clone https://github.com/VahidGh/ChannelWorm.git --branch change_web_app_package_name#65
+
+# cp -r ChannelWorm/django/* ~/app-root/runtime/repo
+
