4d188332ecc08a464026fcba67318117d2401f21
Joe Bowen <joe.b.bowen@gmail.com>
2015-06-17 07:11:11 -0600

    Fix web app. #79

--- channelworm/web_app/urls.py
+++ channelworm/web_app/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.defaults import patterns
 
 base_urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
