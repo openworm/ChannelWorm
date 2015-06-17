978b25d0c655be06b688117d9a5e38110d4c1a84
Joe Bowen <joe.b.bowen@gmail.com>
2015-06-17 15:37:31 -0600

    Fixed cron job to not write over db each sync.

--- channelworm/.openshift/cron/minutely/updated_from_git.sh
+++ channelworm/.openshift/cron/minutely/updated_from_git.sh
@@ -6,6 +6,8 @@ rm -rf ChannelWorm
 
 git clone https://github.com/VahidGh/ChannelWorm.git
 
+rm ChannelWorm/channelworm/db.sqlite3
+
 cp -r ChannelWorm/channelworm/* ~/app-root/runtime/repo
 cp -r ChannelWorm/channelworm/.openshift/* ~/app-root/runtime/repo/.openshift
 
