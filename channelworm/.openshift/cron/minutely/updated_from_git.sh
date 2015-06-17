167e657c7e750572e95cd951e3cf34501cab20da
Joe Bowen <joe.b.bowen@gmail.com>
2015-06-17 15:25:09 -0600

    Update to point the OpenShift cron job to the main repo.

--- channelworm/.openshift/cron/minutely/updated_from_git.sh
+++ channelworm/.openshift/cron/minutely/updated_from_git.sh
@@ -4,7 +4,7 @@ cd ~/app-root/runtime/repo
 
 rm -rf ChannelWorm
 
-git clone https://github.com/joebowen/ChannelWorm.git
+git clone https://github.com/VahidGh/ChannelWorm.git
 
 cp -r ChannelWorm/channelworm/* ~/app-root/runtime/repo
 cp -r ChannelWorm/channelworm/.openshift/* ~/app-root/runtime/repo/.openshift
