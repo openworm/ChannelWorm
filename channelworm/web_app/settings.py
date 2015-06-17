ce643d911a7b34b0a5b77869917f5ec9920e7159
Joe Bowen <joe.b.bowen@gmail.com>
2015-06-17 07:27:05 -0600

    Fix web app. #79

--- channelworm/web_app/settings.py
+++ channelworm/web_app/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/application/static/'
 
 # Pycharm detected this
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
