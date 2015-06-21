81d88cd8f36b6860a83ea6f8745f1a8bcc4c1923
VahidGh <vahidghayoomi@gmail.com>
2015-06-21 07:43:42 +0430

    Fixing makemigrations confirm problem

--- channelworm/.openshift/cron/minutely/updated_from_git.sh
+++ channelworm/.openshift/cron/minutely/updated_from_git.sh
@@ -6,7 +6,7 @@ rm -rf ChannelWorm
 
 git clone https://github.com/VahidGh/ChannelWorm.git
 
-rm ChannelWorm/channelworm/db.sqlite3
+#rm ChannelWorm/channelworm/db.sqlite3
 
 cp -r ChannelWorm/channelworm/* ~/app-root/runtime/repo
 cp -r ChannelWorm/channelworm/.openshift/* ~/app-root/runtime/repo/.openshift
