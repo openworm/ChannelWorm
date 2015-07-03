18dde9341f31bfccaafacad5e2e6d17a677b1f01
Milad <milad.jafary@gmail.com>
2015-07-03 19:26:04 +0430

    Design user registration page. Fixed #119

---
+++ channelworm/account/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    image = models.ImageField(upload_to='account/user/%Y/%m/%d', blank=True)
+
+    def __unicode__(self):
+        return self.user.username
