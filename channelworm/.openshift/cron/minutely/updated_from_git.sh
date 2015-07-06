90b6573b6ced682fc700b4b979b1c71e6c93ff61
VahidGh <vahidghayoomi@gmail.com>
2015-07-07 01:02:39 +0430

    Sync db with media files

--- channelworm/.openshift/cron/minutely/updated_from_git.sh
+++ channelworm/.openshift/cron/minutely/updated_from_git.sh
@@ -7,6 +7,7 @@ rm -rf ChannelWorm
 git clone https://github.com/VahidGh/ChannelWorm.git
 
 rm ChannelWorm/channelworm/db.sqlite3
+rm -rf ChannelWorm/channelworm/web_app/media/*
 
 cp -r ChannelWorm/channelworm/* ~/app-root/runtime/repo
 cp -r ChannelWorm/channelworm/.openshift/* ~/app-root/runtime/repo/.openshift
