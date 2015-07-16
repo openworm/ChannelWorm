4fae31e230fbd5d765f5af6132077462c5a21b41
VahidGh <vahidghayoomi@gmail.com>
2015-07-16 15:08:57 +0430

    Adding G/Gmax / V analysis to cwFitter Adding SLO-2 example Merging IClamp with VClamp

--- channelworm/fitter/Modelator.py
+++ channelworm/fitter/Modelator.py
@@ -14,22 +14,24 @@ class Modelator(object):
         Generates originals vs simulated plots
         """
 
-        #TODO: Add path and GUID to saved plotes
-
+        # TODO: Add path and GUID to save plots
+        
         if 'VClamp' in sampleData:
 
             ref = sampleData['VClamp']['ref']
             for trace in sampleData['VClamp']['traces']:
-                sample_plot, = plt.plot(trace['t'],trace['I'], label = '%i (V)'%trace['vol'])
+                if 'vol' in trace and trace['vol']:
+                    sample_plot, = plt.plot(trace['t'],trace['I'], label = '%i (V)'%trace['vol'])
+                else:
+                    sample_plot, = plt.plot(trace['t'],trace['I'], marker = '.', linestyle='', color='k')
 
-            voltage = self.sim_params['protocol_start']
-            for trace in simData['VClamp']['I']:
-                model_plot, = plt.plot(simData['VClamp']['t'],trace, label = '%i (V)'%voltage)
+            voltage = self.sim_params['protocol_end']
+            for trace in simData['I']:
+                model_plot, = plt.plot(simData['t'],trace, label = '%i (V)'%voltage)
                 voltage -= self.sim_params['protocol_steps']
-
             plt.legend([sample_plot,model_plot], ["Original data from Fig.%s, DOI: %s"%(ref['fig'],ref['doi']),"Best model"])
-            #plt.ylim(-80.0,80.0)
-            #plt.xlim(0.0,1000.0)
+            # plt.ylim(-80.0,80.0)
+            # plt.xlim(0.0,1000.0)
             plt.title("The Best Model fitted to data for voltage-clamp using GA")
             plt.xlabel("Time (s)")
             plt.ylabel("Current (A)")
@@ -43,16 +45,19 @@ class Modelator(object):
 
             ref = sampleData['IClamp']['ref']
             for trace in sampleData['IClamp']['traces']:
-                sample_plot, = plt.plot(trace['t'],trace['V'], label = '%i (A)'%trace['amp'])
+                if 'amp' in trace and trace['amp']:
+                    sample_plot, = plt.plot(trace['t'],trace['V'], label = '%i (A)'%trace['amp'])
+                else:
+                    sample_plot, = plt.plot(trace['t'],trace['V'], marker = '.', linestyle='', color='k')
 
-            amp = self.sim_params['protocol_start_I']
-            for trace in simData['IClamp']['V']:
-                model_plot, = plt.plot(simData['IClamp']['t'],trace, label = '%i (A)'%amp)
-                amp -= self.sim_params['protocol_steps_I']
+            amp = self.sim_params['protocol_end']
+            for trace in simData['V']:
+                model_plot, = plt.plot(simData['t'],trace, label = '%i (A)'%amp)
+                amp -= self.sim_params['protocol_steps']
 
             plt.legend([sample_plot,model_plot], ["Original data from Fig.%s, DOI: %s"%(ref['fig'],ref['doi']),"Best model"])
-            #plt.ylim(-80.0,80.0)
-            #plt.xlim(0.0,1000.0)
+            # plt.ylim(-80.0,80.0)
+            # plt.xlim(0.0,1000.0)
             plt.title("The Best Model fitted to data for current-clamp using GA")
             plt.xlabel("Time (s)")
             plt.ylabel("Voltage (V)")
@@ -65,19 +70,30 @@ class Modelator(object):
         if 'IV' in sampleData:
 
             ref = sampleData['IV']['ref']
-            sample_plot, = plt.plot([round(x*1e3) for x in sampleData['IV']['V']],sampleData['IV']['I'])
-            if 'VClamp' in simData:
-                model_plot, = plt.plot([round(x*1e3) for x in simData['VClamp']['V_max']],simData['VClamp']['I_max'])
-            else:
-                model_plot, = plt.plot([round(x*1e3) for x in simData['IClamp']['V_max']],simData['IClamp']['I_max'])
-
+            sample_plot, = plt.plot([round(x*1e3) for x in sampleData['IV']['V']],sampleData['IV']['I'],'k')
+            model_plot, = plt.plot([round(x*1e3) for x in simData['V_max']],simData['I_max'],'r')
             plt.legend([sample_plot,model_plot], ["Original data from Fig.%s, DOI: %s"%(ref['fig'],ref['doi']),"Best model"])
-            #plt.ylim(-80.0,80.0)
-            #plt.xlim(0.0,1000.0)
+            # plt.ylim(-80.0,80.0)
+            # plt.xlim(0.0,1000.0)
             plt.title("The Best Model fitted to data for I/V curve using GA")
             plt.xlabel("Voltage (mV)")
             plt.ylabel("Current (A/F)")
             plt.savefig("data_vs_candidate-IV.png",bbox_inches='tight',format='png')
+
+            if show:
+                plt.show()
+
+
+        if 'POV' in sampleData:
+
+            ref = sampleData['POV']['ref']
+            sample_plot, = plt.plot([round(x*1e3) for x in sampleData['POV']['V']],sampleData['POV']['PO'],'k')
+            model_plot, = plt.plot([round(x*1e3) for x in simData['V_PO_max']],simData['PO_max'],'r')
+            plt.legend([sample_plot,model_plot], ["Original data from Fig.%s, DOI: %s"%(ref['fig'],ref['doi']),"Best model"])
+            plt.title("The Best Model fitted to data for G/Gmax / V curve using GA")
+            plt.xlabel("Voltage (mV)")
+            plt.ylabel("G/Gmax")
+            plt.savefig("data_vs_candidate-POV.png",bbox_inches='tight',format='png')
 
             if show:
                 plt.show()
