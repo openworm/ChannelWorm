244553c59a62cf9c623b7f20e4837349a80eda91
Travis Jacobs <trav221@gmail.com>
2015-07-23 17:39:32 -0230

    Refactor adapters a lot
    Everything inherits from a generic Adapter class now, so it's easier to
    manage. Still needs a big more kneading, and documentation.

--- channelworm/ion_channel/adapters.py
+++ channelworm/ion_channel/adapters.py
@@ -17,49 +17,11 @@ import ion_channel.models as C
 import PyOpenWorm as P
 from django.forms.models import model_to_dict
 
-class ReferenceAdapter(object):
+class Adapter(object):
     """
-    Map a channelworm Reference object to a PyOpenWorm Experiment.
-    """
+    An interface used to map between PyOpenWorm and ChannelWorm data
+    objects.
 
-    def __init__(self, cw_obj):
-        # initialize PyOpenWorm connection so we can access its API
-        P.connect()
-
-        self.cw = cw_obj
-        self.pyow = P.Evidence(
-            author=self.cw.authors,
-            doi=self.cw.doi,
-            pmid=self.cw.PMID,
-            title=self.cw.title,
-            uri=self.cw.url,
-            year=self.cw.year
-        )
-
-        P.disconnect()
-
-    def get_pow(self):
-        """Return the PyOpenWorm representation of the object"""
-        return self.pyow
-
-    def get_cw(self):
-        """Return the ChannelWorm representation of the object"""
-        return self.cw
-
-
-class PatchClampAdapter(object):
-    """
-    Map a channelworm patch clamp model to a pyopenworm model.
-
-    Example usage ::
-
-        >>> import adapters
-        >>> cw_patch = C.PatchClamp.objects.all()[0]    # get some saved patch-clamp experiment from CW
-        >>> pca = adapters.PatchClampAdapter(cw_patch)    # create an adapter object with it
-        >>> pca.get_pow()    # get back the corresponding PyOW model
-        Experiment(reference=`Evidence(AssertsAllAbout(), year=`None', title=`SALAM', doi=`Salam')', Conditions())
-        >>> pyow_patch = pca.get_pow()    # assign the PyOW to a variable and use it elsewhere
-    
     Attributes
     ----------
 
@@ -72,37 +34,6 @@ class PatchClampAdapter(object):
     cw_obj : The input ChannelWorm object
     """
 
-    def __init__(self, cw_obj):
-        # initialize PyOpenWorm connection so we can access its API
-        P.connect()
-
-        self.channelworm_object = cw_obj
-        cw_dict = model_to_dict(self.channelworm_object)
-
-        experiment_id = cw_dict.pop('experiment')
-        patch_clamp_id = cw_dict.pop('id')
-
-        self.pyopenworm_object = P.Experiment()
-        
-        # get the CW model's experiment
-        cw_evidence = C.Experiment.objects.get(id=experiment_id)
-
-        # make a PyOW evidence object with it
-        pow_evidence = P.Evidence(doi=self.channelworm_object
-                .experiment
-                .reference
-                .doi
-            )
-
-        # add it to the PyOW experiment model
-        self.pyopenworm_object.reference(pow_evidence)
-
-        for key, value in cw_dict.iteritems():
-            self.pyopenworm_object.conditions.set(key, value)
-
-        # we no longer need PyOW API so we can kill the connection
-        P.disconnect()
-
     def get_pow(self):
         """Return the PyOpenWorm representation of the object"""
         return self.pyopenworm_object
@@ -110,3 +41,88 @@ class PatchClampAdapter(object):
     def get_cw(self):
         """Return the ChannelWorm representation of the object"""
         return self.channelworm_object
+
+
+class ReferenceAdapter(Adapter):
+    """
+    Map a channelworm Reference object to a PyOpenWorm Evidence.
+    """
+
+    def __init__(self, cw_obj):
+        # initialize PyOpenWorm connection so we can access its API
+        P.connect()
+
+        self.channelworm_object = cw_obj
+        self.pyopenworm_object = P.Evidence()
+
+        pyow_to_cw = {
+            'author': 'authors',
+            'doi': 'doi',
+            'pmid': 'PMID',
+            'title': 'title',
+            'uri': 'url',
+            'year': 'year',
+        }
+
+        for p, c in pyow_to_cw.items():
+            cw_attr = getattr(self.channelworm_object, c)
+            setattr(self.pyopenworm_object, p, cw_attr)
+
+        P.disconnect()
+
+
+class PatchClampAdapter(Adapter):
+    """
+    Map a channelworm patch clamp model to a pyopenworm model.
+
+    Example usage ::
+
+        >>> import adapters
+        >>> cw_patch = C.PatchClamp.objects.all()[0]    # get some saved patch-clamp experiment from CW
+        >>> pca = adapters.PatchClampAdapter(cw_patch)    # create an adapter object with it
+        >>> pca.get_pow()    # get back the corresponding PyOW model
+        Experiment(reference=`Evidence(AssertsAllAbout(), year=`None', title=`SALAM', doi=`Salam')', Conditions())
+        >>> pyow_patch = pca.get_pow()    # assign the PyOW to a variable and use it elsewhere
+    
+        """
+
+    def __init__(self, cw_obj):
+        # initialize PyOpenWorm connection so we can access its API
+        P.connect()
+
+        self.pyopenworm_object = P.PatchClampExperiment()
+        self.channelworm_object = cw_obj
+
+        #this is just a 1:1 mapping of attributes, but let's spell it
+        # out for consistency
+        pyow_to_cw = {
+            'Ca_concentration': 'Ca_concentration',
+            'Cl_concentration': 'Cl_concentration',
+            'blockers': 'blockers',
+            'cell': 'cell',
+            'cell_age': 'cell_age',
+            'delta_t': 'delta_t',
+            'duration': 'duration',
+            'end_time': 'end_time',
+            'extra_solution': 'extra_solution',
+            'initial_voltage': 'initial_voltage',
+            'ion_channel': 'ion_channel',
+            'membrane_capacitance': 'membrane_capacitance',
+            'mutants': 'mutants',
+            'patch_type': 'patch_type',
+            'pipette_solution': 'pipette_solution',
+            'protocol_end': 'protocol_end',
+            'protocol_start': 'protocol_start',
+            'protocol_step': 'protocol_step',
+            'start_time': 'start_time',
+            'temperature': 'temperature',
+            'type': 'type',
+        }
+ 
+        for p, c in pyow_to_cw.items():
+            cw_attr = getattr(self.channelworm_object, c)
+            setattr(self.pyopenworm_object, p, cw_attr)
+
+        # we no longer need PyOW API so we can kill the connection
+        P.disconnect()
+
