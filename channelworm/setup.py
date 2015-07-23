d995ed968295a582a02926231bd78db3afb0ead8
Travis Jacobs <trav221@gmail.com>
2015-07-23 18:01:30 -0230

    Update channelworm/setup.py

--- channelworm/setup.py
+++ channelworm/setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='ChannelWorm',
+    name='YourAppName',
     # GETTING-STARTED: set your app name:
     version='1.0',
     # GETTING-STARTED: set your app description:
     description='OpenShift App',
     # GETTING-STARTED: set author name (your name):
-    author='',
+    author='Your Name',
     # GETTING-STARTED: set author name (your name):
-    author_email='',
+    author_email='example@example.com',
     # GETTING-STARTED: set author name (your name):
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
-    install_requires=['Django<=1.8','metapub','django-formtools'],
+    install_requires=['Django<=1.8','metapub','django-formtools', 'django-sql-explorer'],
 )
