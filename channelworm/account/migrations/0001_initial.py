0be4394e5adac931e096c141a2bffba33fdff04f
Travis Jacobs <trav221@gmail.com>
2015-07-24 15:11:13 -0230

    Add migrations

---
+++ channelworm/account/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('website', models.URLField(blank=True)),
+                ('image', models.ImageField(upload_to=b'account/user/%Y/%m/%d', blank=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
