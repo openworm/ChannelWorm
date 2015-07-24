f14b9f954ac63fefeca2ad658b2ab5053fe42699
Travis Jacobs <trav221@gmail.com>
2015-07-24 18:18:27 -0230

    Add egg suffix for pyopenworm install dependency

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         'Django<=1.8',
         'PyNeuroML',
+        'PyOpenWorm',
         'cypy',
         'django-formtools',
         'django-sql-explorer',
@@ -28,7 +29,7 @@ setup(
         'git+https://github.com/NeuroML/pyNeuroML.git#egg=PyNeuroML',
         'git+https://github.com/pgleeson/pyelectro.git#egg=pyelectro',
         'git+https://github.com/pgleeson/neurotune.git#egg=neurotune',
-        'git+https://github.com/openworm/PyOpenWorm.git@dev',
+        'git+https://github.com/openworm/PyOpenWorm.git#egg=PyOpenWorm@dev',
         'git+https://github.com/rgerkin/neuronunit.git#egg=neuronunit',
     ]
 )
