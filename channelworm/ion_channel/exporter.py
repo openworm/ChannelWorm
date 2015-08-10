3beec5224358c61b302bc1a8e1a303a5a94620cd
Travis Jacobs <trav221@gmail.com>
2015-08-10 17:42:58 -0230

    Begin defining Exporter for ChannelWorm to PyOW
    Decided on n3 as default format for export.

--- channelworm/ion_channel/exporter.py
+++ channelworm/ion_channel/exporter.py
@@ -1,4 +1,6 @@
-import adapters
+import rdflib
+from adapters import Adapter
+from django.db.models import Model
 
 class Exporter(object):
     """
@@ -7,23 +9,50 @@ class Exporter(object):
     """
     def __init__(self, *args):
         self.data = []
+        self.graph = rdflib.Graph()
+        self.exported = False
         if args:
             self.load(*args)
 
     def load(self, *args):
         """
-        Load data into the Exporter.
+        Load new ChannelWorm data into the Exporter.
         """
         for item in args:
-            print(item)
+            if not isinstance(item, Model):
+                raise TypeError(
+                    'Item {} is not instance of {}.'
+                    .format(item, type(Model))
+                )
             self.data.append(item)
 
-    def export(self, filename):
+    def parse(self, source=None, format='n3'):
+        """
+        Load data from the previous graph into the Exporter.
+        """
+        if not source:
+            raise TypeError('No data source specified.')
+        self.graph.parse(
+            source=source,
+            format=format
+        )
+
+    def export(self, filename=None, format='n3'):
         """
         Export the ChannelWorm data to a flat file that can be
         consumed by PyOpenWorm.
         """
-        for item in self.data:
-            # call adapter on each item
-            # do something with resulting PyOW objects
-            pass # (for now)
+        if not self.exported:
+            for item in self.data:
+                item_adapter = Adapter.create(item)
+                pyow_object = item_adapter.get_pow()
+                trips = pyow_object.triples()
+                for triple in trips:
+                    self.graph.add(trip)
+            self.exported = True
+
+        self.graph.serialize(
+            destination=filename,
+            format=format
+        )
+
