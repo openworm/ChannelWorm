0b1c1f49b71785f5333e3c86d2ed97832d75a8f5
Richard C Gerkin <rick1@gerkin.org>
2016-06-23 20:02:12 -0700

    Update notebook_test.py

--- tests/notebook_test.py
+++ tests/notebook_test.py
@@ -8,9 +8,10 @@ def test_notebook_runs():
     Converts the version controlled notebook to a python script, 
     and then runs that script.  
     """
-    notebook_name = 'SCU_IVCurve_Model'
-    code = subprocess.call('ipython nbconvert %s.ipynb --to python' % notebook_name, shell=True)
-    assert(code == 0, "Could not create python script from testing notebook %s.ipynb" % notebook_name)
-    code = subprocess.call([sys.executable, 'SCU_IVCurve_Model.py'],
-                            cwd='tests')
-    assert(code == 0, "The test script %s.py raised an error" % notebook_name)
+    notebooks = ['EGL-19_IV','SLO-2_IV']
+    for notebook_name in notebooks:
+        code = subprocess.call('ipython nbconvert sciunit/%s.ipynb --to python' % notebook_name, shell=True)
+        assert(code == 0, "Could not create python script from testing notebook %s.ipynb" % notebook_name)
+        code = subprocess.call([sys.executable, '%s.py' % notebook_name],
+                                cwd='tests')
+        assert(code == 0, "The test script %s.py raised an error" % notebook_name)
